026ccbb9bc38b5f898b557bd38c0be09ab3f078d
Hemaraj Sampath kumar <hemaraj@Hemarajs-MacBook-Pro.local>
2018-08-05 00:05:58 -0400

    added feautures for webotron project

---
+++ 01-webotron/webotron/webotron.py
@@ -0,0 +1,28 @@
+import boto3
+import click
+
+
+session = boto3.Session(profile_name='pythonAutomation')
+s3 = session.resource("s3")
+
+@click.group()
+
+def cli():
+    "Webotron deploys websites to AWS"
+    pass
+
+@cli.command('list_buckets')
+def list_buckets():
+    "list all buckets"
+    for bucket in s3.buckets.all():
+        print(bucket)
+
+@cli.command('list_bucket_objects')
+@click.argument('bucket')
+def list_bucket_objects(bucket):
+    "List all objects in the bucket"
+    for obj in s3.Bucket(bucket).objects.all():
+        print(obj)
+
+if __name__ == '__main__':
+    cli()
--- README.md
+++ README.md
@@ -7,3 +7,11 @@ Repo for the project automating-aws-with-python
 ##  01-Webotron
 
 Webotron is a script that will sync a local directory to an s3 bucket and optionally configure Route 53 and cloudFront as well.
+
+
+### Features
+
+webotron currently has the follwoing Features
+
+  -->list_buckets
+  -->list_bucket_objects
